fdcb6d8c261abb45fc60dc762db06ed73891ccb6
Yarko Tymciurak <yarkot1@gmail.com>
2017-12-08 11:39:31 -0600

    update for simpler mp4 conversion

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -10,12 +10,14 @@ CVT=HandBrakeCLI
 # everything needed is now in the config json:
 # hb-audio-match based on -Z Normal and manually matching audio bitrate of sources
 OPTS="--preset-import-file hb-audio-match.json"
+OPTS="-B 16 -O -I"
+
 FROM_D=original_videos
 TO_D=mp4_converted
 
 for from in $FROM_D/*.webm; do
-    to=$(basename ${from%.*}).m4v
-    echo $CVT $OPTS -i "$from" -o "$TO_D/$to": | tee convert.log
-    $CVT $OPTS -i "$from" -o "$TO_D/$to" 2>> convert.log
+    to=$(basename ${from%.*}).mp4
+    echo $CVT $OPTS -i "$from" -o "$TO_D/$to": | tee $TO_D/convert.log
+    $CVT $OPTS -i "$from" -o "$TO_D/$to" 2>> $TO_D/convert.log
 done
 
