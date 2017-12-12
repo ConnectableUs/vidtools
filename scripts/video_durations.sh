6f57f7240223e9f7877ecf5f8dc3ee4256ac6da3
Yarko Tymciurak <yarkot1@gmail.com>
2017-12-12 14:23:36 -0600

    typo;

--- scripts/video_durations.sh
+++ scripts/video_durations.sh
@@ -9,5 +9,5 @@ echo $DIRNAME:
 for i in $DIRNAME/*; do
     # only look at files with EXT extension
     [ "${i##*.}" = "$EXT" ] || continue
-    ffmpeg -i "$i" 2>&1 | { echo -n {$i##*/}; fgrep Duration; }
+    ffmpeg -i "$i" 2>&1 | { echo -n ${i##*/}; fgrep Duration; }
 done
